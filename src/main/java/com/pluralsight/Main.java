4c1e39eb22ed062f3859ba9c18c73d2d2008a4c1
polinabreha <polinabreha@gmail.com>
2026-05-05 08:19:15 -0400

    testing the program in the main

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -2,8 +2,42 @@ package com.pluralsight;
 
 public class Main {
     public static void main(String[] args) {
+        room();
+        reservation();
+        employee();
 
+    }
 
+    public static void room(){
+        Room room = new Room(2,150.00);
+        System.out.println("Available: " + room.isAvailable()); // false
+        room.checkOut();
+        room.cleanRoom();
+        System.out.println("Available: " + room.isAvailable()); // true
 
+        Reservation res = new Reservation("King", 3, true);
+        System.out.printf("Total: %.2f%n" , res.getReservationTotal());
+    }
+
+    public static void reservation(){
+        Reservation reservation = new Reservation("King", 3, true);
+        System.out.println("Is weekend? " + reservation.isWeekend());
+        System.out.println("Number of nights: " + reservation.getNumberOfNights());
+        System.out.println("Your room type is: " + reservation.getRoomType());
+        System.out.printf("Start price: %.2f%n" , reservation.getPrice());
+        System.out.printf("End price: %.2f%n" , reservation.getReservationTotal());
+
+    }
+    public static void employee(){
+        Employee emp = new Employee(1, "John", "Doe", "IT");
+        emp.setPayRate(20.00);
+
+        emp.punchIn(9);
+        emp.punchOut(17);
+
+        System.out.println("Hours Worked: " + emp.getHoursWorked());
+        System.out.println("Regular Hours: " + emp.getRegularHours());
+        System.out.println("Overtime Hours: " + emp.getOvertimeHours());
+        System.out.println("Total Pay: " + emp.getTotalPay());
     }
 }
